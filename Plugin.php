e4382aa55ccf037b5aac144cb0ef885f766b6a3d
Frederic Guillot <fred@kanboard.net>
2016-06-01 21:38:56 -0400

    Added Acl for webhook controller

--- Plugin.php
+++ Plugin.php
@@ -3,6 +3,7 @@
 namespace Kanboard\Plugin\GogsWebhook;
 
 use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Security\Role;
 use Kanboard\Core\Translator;
 
 class Plugin extends Base
@@ -11,10 +12,9 @@ class Plugin extends Base
     {
         $this->actionManager->getAction('\Kanboard\Action\CommentCreation')->addEvent(WebhookHandler::EVENT_COMMIT);
         $this->actionManager->getAction('\Kanboard\Action\TaskClose')->addEvent(WebhookHandler::EVENT_COMMIT);
-
         $this->template->hook->attach('template:project:integrations', 'GogsWebhook:project/integrations');
-
         $this->route->addRoute('/webhook/gogs/:project_id/:token', 'WebhookController', 'handler', 'GogsWebhook');
+        $this->applicationAccessMap->add('WebhookController', 'handler', Role::APP_PUBLIC);
     }
 
     public function onStartup()
