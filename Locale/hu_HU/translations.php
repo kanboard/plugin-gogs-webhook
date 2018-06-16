1e20c2d0c947e83178f5d7f8441371f356c7bfc7
Balázs Úr <urbalazs@gmail.com>
2018-06-16 17:41:30 +0200

    Updated Hungarian translation

---
+++ Locale/hu_HU/translations.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'Gogs webhooks' => 'Gogs webhorgok',
+    'Help on Gogs webhooks' => 'Súgó a Gogs webhorgokhoz',
+    'Gogs commit received' => 'Gogs kommit érkezett',
+    'Bind Gogs webhook events to Kanboard automatic actions' => 'Gogs webhorog események kötése a Kanboard automatikus műveleteihez',
+    'Commit made by @%s on Gogs' => '@%s által készített kommit a Gogs oldalon',
+);
